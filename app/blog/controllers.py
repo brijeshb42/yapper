56623cc2bda8563fa6fca607583d91ac7c8f3611
Brijesh Bittu <brijeshb42@gmail.com>
2014-11-23 17:44:06 +0530

    Updated blog controller to throw 404 error

--- app/blog/controllers.py
+++ app/blog/controllers.py
@@ -14,6 +14,7 @@ def get_post(pid):
     post = Post.query.get_or_404(pid)
     return render_template('blog/index.html', posts=[post], pagination=None)
 
+
 @blog_blueprint.route('/page/<int:page>')
 @blog_blueprint.route('/')
 def index(page=1):
---
+++ tests/AppTest/test_login.py
@@ -0,0 +1,62 @@
+"""
+(c) 2014 by Brijesh Bittu
+"""
+import unittest
+from flask import current_app
+from app import create_app, db
+from app.user.models import Role, User
+from app.blog.models import Post
+
+class LoginTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('test')
+        self.app.config['CSRF_ENABLED'] = False
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        role = Role(name='admin')
+        db.session.add(role)
+        db.session.commit()
+        user = User(name='Test User', email='test@mail.com',\
+                password='testpass', role=role)
+        db.session.add(user)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def login(self, e, p):
+        return self.client.post('/u/login', data=dict(
+            email=e,
+            password=p
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.client.get('/u/logout', follow_redirects=True)
+
+    def test_empty_db(self):
+        p = Post.query.all()
+        assert len(p) is 0
+
+    def test_can_login(self):
+        rv = self.login('test@mail.com','testpass')
+        assert 'Test User' in rv.data
+        rv = self.logout()
+        assert 'logged out' in rv.data
+        rv = self.login('tets@gmail.com','po')
+        assert 'Invalid combination' in rv.data
+
+    def test_add_post(self):
+        self.login('test@mail.com','testpass')
+        rv = self.client.post('/blog/add', data=dict(
+            title='Title',
+            body='## hello'
+            ), follow_redirects=True)
+        assert '<h2>hello</h2>' in rv.data
+
+        rv = self.client.get('/blog/12')
+        assert rv.status_code == 404
+
